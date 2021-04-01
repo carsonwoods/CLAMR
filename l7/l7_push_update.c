e382f176ec4bc04ef47549dd27b7d18fca8dba06
Justin Broaddus <dzt972@mocs.utc.edu>
2021-04-01 06:31:16 -0700

    Added MPI_Barrier to l7_push_update.c

--- l7/l7_push_update.c
+++ l7/l7_push_update.c
@@ -159,6 +159,7 @@ int L7_Push_Update(
    MPI_Status  status[2*l7_push_id_db->num_comm_partners];
 
    int iloc = 0;
+   MPI_Barrier(MPI_COMM_WORLD);
    for (int ip = 0; ip < l7_push_id_db->num_comm_partners; ip++){
       MPI_Irecv(&return_array[iloc], l7_push_id_db->recv_buffer_count[ip], MPI_INT,
                 l7_push_id_db->comm_partner[ip], l7_push_id_db->comm_partner[ip], MPI_COMM_WORLD, &request[ip]);
